6492b728e971d0d1dffb822c8b22d2e4ee85eadc
Mitansh Prajapati <mitanshp2112@gmail.com>
2026-01-28 14:27:25 +0530

    Y2+Y7 logics

--- backend/plc/test.py
+++ backend/plc/test.py
@@ -1,9 +1,16 @@
-import requests
-import sys
+try:
+    import requests
+    import sys
 
-url = "http://localhost:5001/plc/write"
-payload = {
-    "device": "Y1",
-    "value": 1
-}
-response = requests.post(url, json=payload)
+    url = "http://localhost:5001/plc/write"
+    payload = {
+        "device": "M77",
+        "value": 1
+    }
+    response = requests.post(url, json=payload)
+    print(response.json())
+except:
+    import rk_mcprotocol as mc
+    sock= mc.open_socket('192.168.1.30',5000)
+    mc.write_bit(sock,'Y1',[1])
+    print(mc.read_bit(sock,'Y1',1))
